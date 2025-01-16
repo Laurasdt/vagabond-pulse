299ff17eb0aa5fc2af5806855bfe071e607bb02a
Laura Sdt <lauramarie.sdt@gmail.com>
2025-01-16 17:22:41 +0100

    first steps on the header

---
+++ src/components/Header.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import "./Header.css";
+
+function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  // Toggle ouvert/fermé
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <header className="header">
+      <h1>Vagabond Pulse</h1>
+      <button className="burger-icon" onClick={toggleMenu}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </button>
+      {/* // Menu mobile first */}
+      <nav className={`nav-links ${isMenuOpen ? "open" : ""}`}>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Profile</a>
+          </li>
+          <li>
+            <a href="#">Events</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
