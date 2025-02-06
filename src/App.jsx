3782ad275625a095fe7e87004edb3b6f05507501
Laura Sdt <lauramarie.sdt@gmail.com>
2025-02-06 09:20:38 +0100

    first step on the Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
 import React from "react";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import "./App.scss";
 
-/*************  ✨ Codeium Command ⭐  *************/
-/**
- * The App component serves as the root component of the application.
- * It renders the Header component as part of the main application structure.
- */
-
-/******  63b542b3-e3c4-4c76-b0c7-d471afc2a9a6  *******/
 function App() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="links">
+        <a href="#">Politique de confidentialité</a>
+        <a href="#">Mentions légales</a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
